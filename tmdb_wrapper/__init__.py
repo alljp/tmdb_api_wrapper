0bd21cd8c788e4e19adef9566805061b27fc42ed
alljp <allenjosesmailbox@gmail.com>
2017-08-14 00:17:56 +0530

    Add code to read API_key from config.ini file

---
+++ tmdb_wrapper/__init__.py
@@ -0,0 +1,24 @@
+import os
+import requests
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+BASE_URL = "https://api.themoviedb.org/3/"
+API_KEY = config["API"]["Key"]
+
+
+class APIKeyMissingError(Exception):
+    pass
+
+
+if not API_KEY:
+    raise APIKeyMissingError(
+        "API key missing! All methods require an API key.")
+session = requests.Session()
+session.params = {}
+session.params['api_key'] = API_KEY
+
+from .tv import TV
