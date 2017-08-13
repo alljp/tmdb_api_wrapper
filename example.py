84137ce9ce5deb187c60dff6336b0836e22b4aa6
alljp <allenjosesmailbox@gmail.com>
2017-08-14 00:41:44 +0530

    Add Movie.popular() to example.

--- example.py
+++ example.py
@@ -1,10 +1,17 @@
 from __future__ import print_function
 from tmdb_wrapper import TV
+from tmdb_wrapper import Movie
+from tmdb_wrapper import Find
 
 popular = TV.popular()
-info = TV.info
-# print(info(1403))
 
+print("Popular TV shows")
 for number, show in enumerate(popular['results'], start=1):
     print("{}. {} - {}".format(
         number, show['name'], show['popularity']))
+
+popular_movies = Movie.popular()
+print(popular_movies)
+print("\n\nPopular Movies")
+for number, movie in enumerate(popular_movies['results'], start=1):
+    print("{}. {} - {}".format(number, movie['title'], movie['popularity']))
--- tmdb_wrapper/movie.py
+++ tmdb_wrapper/movie.py
@@ -8,11 +8,10 @@ class Movie(object):
 
     def info(id):
         path = 'https://api.themoviedb.org/3/movie/{}'.format(id)
-        # path = 'https://api.themoviedb.org/3/tv/1403'
         response = session.get(path)
         return response.json()
 
     def popular():
-        path = 'https://api.themoviedb.org/3/movie/popular&page=2'
+        path = 'https://api.themoviedb.org/3/movie/popular'
         response = session.get(path)
         return response.json()
